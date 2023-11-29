a4877e341c3b16200988e338db2b87c1741234a8
Junil Ko <junil@skkukdp.re.kr>
2023-11-29 13:19:06 +0900

    GameManager Switch문 추가

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -9,8 +9,132 @@ public class GameManager : MonoBehaviour
     public TMP_Text displayText;
 
     // 버튼 클릭 이벤트
-    public void OnClickButton()
+    public void OnClickButton(string buttonStr)
     {
-        displayText.text += "1";
+        switch (buttonStr)
+        {
+            case "0":
+                if (displayText.text == "0")
+                {
+                    displayText.text = buttonStr;
+                }
+                else
+                {
+                    displayText.text += buttonStr;
+                }
+                break;
+            case "1":
+                if (displayText.text == "0")
+                {
+                    displayText.text = buttonStr;
+                }
+                else
+                {
+                    displayText.text += buttonStr;
+                }
+                break;
+            case "2":
+                if (displayText.text == "0")
+                {
+                    displayText.text = buttonStr;
+                }
+                else
+                {
+                    displayText.text += buttonStr;
+                }
+                break;
+            case "3":
+                if (displayText.text == "0")
+                {
+                    displayText.text = buttonStr;
+                }
+                else
+                {
+                    displayText.text += buttonStr;
+                }
+                break;
+            case "4":
+                if (displayText.text == "0")
+                {
+                    displayText.text = buttonStr;
+                }
+                else
+                {
+                    displayText.text += buttonStr;
+                }
+                break;
+            case "5":
+                if (displayText.text == "0")
+                {
+                    displayText.text = buttonStr;
+                }
+                else
+                {
+                    displayText.text += buttonStr;
+                }
+                break;
+            case "6":
+                if (displayText.text == "0")
+                {
+                    displayText.text = buttonStr;
+                }
+                else
+                {
+                    displayText.text += buttonStr;
+                }
+                break;
+            case "7":
+                if (displayText.text == "0")
+                {
+                    displayText.text = buttonStr;
+                }
+                else
+                {
+                    displayText.text += buttonStr;
+                }
+                break;
+            case "8":
+                if (displayText.text == "0")
+                {
+                    displayText.text = buttonStr;
+                }
+                else
+                {
+                    displayText.text += buttonStr;
+                }
+                break;
+            case "9":
+                if (displayText.text == "0")
+                {
+                    displayText.text = buttonStr;
+                }
+                else
+                {
+                    displayText.text += buttonStr;
+                }
+                break;
+            case ".":
+                if (displayText.text == "0")
+                {
+                    displayText.text = buttonStr;
+                }
+                else
+                {
+                    displayText.text += buttonStr;
+                }
+                break;
+            case "c":
+                break;
+            case "+":
+                break;
+            case "-":
+                break;
+            case "*":
+                break;
+            case "/":
+                break;
+            case "=":
+                break;
+        }
     }
 }
